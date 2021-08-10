6660ad301d92eaea8431c079253fbc3de1905a2c
zganatra <65689529+zganatra@users.noreply.github.com>
2021-08-10 18:45:50 -0400

    Update train_lgbm.py

--- ai-platform/src/train_lgbm.py
+++ ai-platform/src/train_lgbm.py
@@ -25,4 +25,31 @@ parameters = {
 
 run = wandb.init(project="aiplatform-lgbm-project", config=parameters)
 
-actual_execution()
+train = pd.read_csv('gs://test-zganatra/kraggle_lgbm_data/train.csv')
+y = train.target.values
+train.drop(['id', 'target'], inplace=True, axis=1)
+x = train.values
+
+x, x_test, y, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
+
+categorical_features = [c for c, col in enumerate(train.columns) if 'cat' in col]
+train_data = lightgbm.Dataset(x, label=y, categorical_feature=categorical_features)
+test_data = lightgbm.Dataset(x_test, label=y_test)
+
+categorical_features = [c for c, col in enumerate(train.columns) if 'cat' in col]
+train_data = lightgbm.Dataset(x, label=y, categorical_feature=categorical_features)
+test_data = lightgbm.Dataset(x_test, label=y_test)
+
+model = lightgbm.train(parameters,
+                     train_data,
+                     valid_sets=test_data,
+                     num_boost_round=5000,
+                     early_stopping_rounds=100,
+                     callbacks=[wandb_callback()])
+
+submission = pd.read_csv('gs://test-zganatra/kraggle_lgbm_data/test.csv')
+ids = submission['id'].values
+
+submission.drop('id', inplace=True, axis=1)
+x = submission.values
+y = model.predict(x)
